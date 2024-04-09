10d342210d74a569c3d479dce83fe65474b501a9
Aleksei Lapunov <lapunoff.alex@yandex.ru>
2024-04-10 01:44:36 +0700

    Defined Material types

--- ConsoleSand/materials.hpp
+++ ConsoleSand/materials.hpp
@@ -1,19 +1,21 @@
 #include "common_utils.hpp"
 
-typedef enum class MaterialId { Air = 0, Sand, Water, Stone, Gas };
+typedef enum class MaterialId   { Air = 0, Sand, Water, Stone, Gas };
+typedef enum class MaterialType { Void = 0, Solid, Powdery, Liquid, Gas };
 
 struct Material
 {
 	char symbol;
 	std::string color;
 	std::string displayName;
+	MaterialType type;
 };
 
 static const std::unordered_map<MaterialId, Material> materials
 {
-  {MaterialId::Air,		{AIR_SIGN,		BLACK,		"Air"}},
-  {MaterialId::Sand,	{SAND_SIGN,		YELLOW_BR,	"Sand"}},
-  {MaterialId::Water,	{WATER_SIGN,	BLUE_BR,	"Water"}},
-  {MaterialId::Stone,	{STONE_SIGN,	BLACK_BR,	"Stone"}},
-  {MaterialId::Gas,		{GAS_SIGN,		GREEN,		"Gas"}}
+  {MaterialId::Air,		{AIR_SIGN,		BLACK,		"Air",		MaterialType::Void}},
+  {MaterialId::Sand,	{SAND_SIGN,		YELLOW_BR,	"Sand",		MaterialType::Powdery}},
+  {MaterialId::Water,	{WATER_SIGN,	BLUE_BR,	"Water",	MaterialType::Liquid}},
+  {MaterialId::Stone,	{STONE_SIGN,	BLACK_BR,	"Stone",	MaterialType::Solid}},
+  {MaterialId::Gas,		{GAS_SIGN,		GREEN,		"Gas",		MaterialType::Gas}}
 };
